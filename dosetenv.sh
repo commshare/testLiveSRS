f11ba8c94cabdf24ae93acbeb60fc3b0cd7cec57
commshare <commshare.zhang@gmail.com>
2015-09-25 14:53:48 +0800

    build and run srs on ns115 debian wheezy ok

---
+++ dosetenv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export SrsArmCC="arm-linux-gnueabihf-gcc" &&
+    export  SrsArmGCC="arm-linux-gnueabihf-gcc" &&
+    export SrsArmCXX="arm-linux-gnueabihf-g++" &&
+    export SrsArmAR="arm-linux-gnueabihf-ar" &&
+    export SrsArmLD="arm-linux-gnueabihf-ld" &&
+    export SrsArmRANDLIB="arm-linux-gnueabihf-ranlib"
+
+
+echo "set build env done ..."
